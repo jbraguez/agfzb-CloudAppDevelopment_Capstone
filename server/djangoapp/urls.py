c6078e07de4294e0245447d17d6039660c2a373f
jbraguez <jbargeuz@gmail.com>
2023-09-16 06:49:51 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,16 +3,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
+    path(route='/', view=views.django, name='django'),
     # name the URL
-
+    
     # path for about view
-
+    path('about/', views.about, name='about'),
     # path for contact us view
-
+    path('contact/', views.contact, name='contact'),
     # path for registration
 
     # path for login
